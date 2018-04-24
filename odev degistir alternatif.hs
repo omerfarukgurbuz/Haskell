335d2e87235c873e9154a09d5cfb039477546fba
OmerFarukGurbuz <omerf2925@gmail.com>
2018-04-24 18:32:08 +0300

    benim odev

--- odev degistir alternatif.hs
+++
@@ -1,4 +0,0 @@
-cift list1=[x|x<-[0,2..((length list1)-1)]]
-olustur list1=[if((length list1)==(x+1))
-               then  [list1!!x]
-               else  [list1!!(x+1),list1!!x]|x<-(cift list1)]
--- odevstrchange.hs
+++
@@ -1,6 +0,0 @@
-strchange [] = []
-strchange (j:jk) = do
-if [j] == "a" then "b" ++ strchange (jk) 
-else if [j] == "b"then "c" ++ strchange (jk) 
-else if [j] == "c" then "a" ++ strchange (jk) 
-else [j] ++ strchange(jk)
